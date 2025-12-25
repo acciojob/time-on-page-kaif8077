2a3dd5a8a0c5803338fe67065161dcddc05f0a02
Mohammad Kaif <mohdkaif90275@gmail.com>
2025-12-25 22:05:08 +0530

    kaif

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,8 @@ const App = () => {
   
   return (
       <div>
-        <p>You've  been on this page for {count} seconds</p>
+        <p>You've been on this page for {count} seconds.</p>
+
       </div>
     )
   }
